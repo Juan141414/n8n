6aca5e428a4d487e4aefe4758ed3bdf365c7709f
Juan141414 <jisanjuanor@yahoo.es>
2025-12-17 12:54:35 +0100

    Delete terraform/n8n_infrastructure directory

--- terraform/n8n_infrastructure/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "n8n_test_bucket" {
-  bucket = "n8n-test-bucket-extended"
-  acl    = "private"
-
-  tags = {
-    Name        = "n8n-test-bucket-extended"
-    Environment = "Test"
-  }
-}
