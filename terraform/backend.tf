7e806b7f1fc7f04c008332e602ada9ca4fdcff49
Juan141414 <jisanjuanor@yahoo.es>
2025-12-17 19:02:09 +0100

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "n8n-test-bucket-extended"
-    region = "us-east-1"
-  }
-}
--- terraform/subnets.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
